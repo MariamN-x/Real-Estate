0fd141558882f1009145ce5111d2f0ec35b6925b
ayahisha <149316170+ayahisha@users.noreply.github.com>
2024-04-30 12:33:12 +0300

    Add files via upload
    Home page

--- js.js
+++ js.js
@@ -1,15 +1,17 @@
+const scrollContainer = document.querySelector('.scroll-container');
+const scrollButtonPrev = document.querySelector('.scroll-button.prev');
+const scrollButtonNext = document.querySelector('.scroll-button.next');
 
+scrollButtonPrev.addEventListener('click', () => {
+    scrollContainer.scrollBy({
+        left: -300, // Adjust as needed
+        behavior: 'smooth'
+    });
+});
 
-document.addEventListener("DOMContentLoaded", function() {
-    const sortBtn = document.querySelector(".sort-btn");
-  
-    sortBtn.addEventListener("mouseover", function() {
-      sortBtn.style.backgroundColor = "white";
-      sortBtn.style.color = "black";
+scrollButtonNext.addEventListener('click', () => {
+    scrollContainer.scrollBy({
+        left: 300, // Adjust as needed
+        behavior: 'smooth'
     });
-  
-    sortBtn.addEventListener("mouseout", function() {
-      sortBtn.style.backgroundColor = "black";
-      sortBtn.style.color = "white";
-    });
-  });
+});
